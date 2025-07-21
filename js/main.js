ad8e7a2223a81287607651aa11bfd051c89847d7
Michael Shpeh <mspeg236@gmail.com>
2025-07-21 15:05:08 +0300

    arrangement start

--- js/main.js
+++ js/main.js
@@ -30,3 +30,30 @@ document.getElementById("btn-uk").addEventListener("click", () => {
 document.getElementById("btn-en").addEventListener("click", () => {
   setLanguage("en");
 });
+
+const btnOpn = document.querySelector(".arrangement__button");
+const backdrop = document.querySelector(".backdrop");
+const btnCls = document.querySelector(".btnclose");
+
+const opensModal = function () {
+    backdrop.classList.remove("is-hidden");
+};
+
+const closesModal = function () {
+    backdrop.classList.add("is-hidden")
+};
+
+btnOpn.addEventListener("click", opensModal);
+btnCls.addEventListener("click", closesModal); 
+
+backdrop.addEventListener("click", function (e) {
+  if (e.target === backdrop) {
+    closesModal();
+  }
+});
+
+document.addEventListener("keydown", function (e) {
+  if (e.key === "Escape") {
+    closesModal();
+  }
+});
